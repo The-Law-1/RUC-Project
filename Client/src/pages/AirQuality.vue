2f5c6b67c81d6fcc7f4062b186dd9d19589089d1
Gabriel <gabriel.griffin@epitech.eu>
2023-05-24 17:37:47 +0200

    ✨ Implemented charts and colours for good/bad values

--- Client/src/pages/AirQuality.vue
+++ Client/src/pages/AirQuality.vue
@@ -17,6 +17,15 @@
                 CO2
             </div>
             <!-- Graphique CO2 -->
+            <div class="w-full">
+                  <Bar
+                        id="CO2-chart"
+                        :options="{
+                            responsive: true
+                        }"
+                        :data="CO2Data"
+                    />
+            </div>
         </div>
 
         <div>
@@ -42,6 +51,54 @@
     </div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
+import { ref, defineComponent } from 'vue';
+import type { QualityTimestamp } from "@/types/QualityTimestamp";
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+  
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+var dummyCO2Values = ref([] as QualityTimestamp[]);
+
+var minutesInterval = 10;
+
+// fill array with dummy values, taken every *minutesInterval* minutes
+for (let i = 0; i < 24 * 60; i += minutesInterval) {
+    dummyCO2Values.value.push({
+        // midnight of may 1st + minute values i
+        date: new Date(2023, 5, 1, 0, i),
+        value: Math.floor(Math.random() * 100)
+    });
+}
+
+// extract a labels array and datasets array
+const labels = dummyCO2Values.value.map((value) => value.date.toLocaleTimeString());
+// array of colours from values, red if value is < 25 or > 75, green if not
+const colors = dummyCO2Values.value.map((value) => value.value < 25 || value.value > 75 ? '#f87979' : '#41b883');
+const datasets = [
+    {
+        label: 'CO2',
+        data: dummyCO2Values.value.map((value) => value.value),
+        backgroundColor: colors
+    }
+];
+
+const CO2Data = ref({
+    labels,
+    datasets,
+});
+
+export default defineComponent({
+  name: 'AirQualityPage',
+  components: { Bar },
+    setup() {
+        return {
+        dummyCO2Values,
+        CO2Data
+        }
+    }
+});
+
 
 </script>
