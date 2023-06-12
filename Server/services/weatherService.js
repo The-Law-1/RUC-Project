2183459994a809bd2b60281e783e5ea24506d44d
Gabriel <gabriel.griffin@epitech.eu>
2023-06-12 13:51:14 +0200

    ✨ Adding current weather to payload

--- Server/services/weatherService.js
+++ Server/services/weatherService.js
@@ -2,11 +2,13 @@ class WeatherService {
 
     async getWeather() {
         // trekroner 58.06, 7.98
-        const url = "https://api.open-meteo.com/v1/forecast?latitude=58.06&longitude=7.98&hourly=temperature_2m";
+        const url = "https://api.open-meteo.com/v1/forecast?latitude=58.06&longitude=7.98&hourly=temperature_2m&daily=weathercode,sunrise,sunset&current_weather=true&timezone=Europe%2FBerlin";
 
         const response = await fetch(url);
 
         const data = await response.json();
+
+        // maybe just return today/now
 
         return data;
     }
